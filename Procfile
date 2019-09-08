11c7c029623971452a3f6142a03ef1f4cb48e000
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-09-08 19:07:22 +0200

    Cleanup clojure stuff, leaving clojurescript + shadow only

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/mftickets-web.jar clojure.main -m mftickets-web.server
--- env/prod/cljs/mftickets_web/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns mftickets-web.prod
-  (:require [mftickets-web.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
