90fbc2e93b94dbe1efddcb33300c0b73f4c5009e
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-10-12 15:18:57 +0200

    OPT Uses defrecord for all ping handlers

--- src/cljs/mftickets_web/components/life_prober/handlers.cljs
+++ src/cljs/mftickets_web/components/life_prober/handlers.cljs
@@ -6,21 +6,14 @@
    [mftickets-web.events :as events]
    [mftickets-web.events.protocols :as events.protocols]))
 
-(defn- after-ping
-  "Events that represents that the ping ended."
-  [_ response]
+(defrecord Ping--after [response]
+  events.protocols/PEvent
+  (reduce! [_] (reducers/after-ping response)))
 
-  (reify events.protocols/PEvent
-
-    (reduce! [_] (reducers/after-ping response))))
-
-(defn ping
-  "Event to ping the server and set the response status."
-  [{{:keys [ping]} :http :as props}]
-
-  (reify events.protocols/PEvent
-
-    (reduce! [_] (reducers/before-ping))
-
-    (run-effects! [_] (async/go [(->> (ping) async/<! (after-ping props))]))))
+(defrecord Ping [props]
+  events.protocols/PEvent
+  (reduce! [_] (reducers/before-ping))
+  (run-effects! [_]
+    (let [ping (-> props :http :ping)]
+      (async/go [(->> (ping) async/<! ->Ping--after)]))))
 
